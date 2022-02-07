d68fd58ae71f74c880f8b6a448a038974a74138e
mnvr00 <munevveroguz1@gmail.com>
2022-02-07 19:28:19 +0000

    my initial commit...

--- src/test/java/com/practice/EU7/NewMunevver.java
+++ src/test/java/com/practice/EU7/NewMunevver.java
@@ -3,5 +3,6 @@ package com.practice.EU7;
 public class NewMunevver {
     public static void main(String[] args) {
         System.out.println("we are from group 21  ");
+        System.out.println("munevver");
     }
 }
