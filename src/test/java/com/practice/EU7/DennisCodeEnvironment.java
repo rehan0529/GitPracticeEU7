14ed12523eb75eaf6869b508c08e15417db7529c
EnesSatin <enessatin@gmail.com>
2022-02-07 19:20:14 +0000

    My second step !!

---
+++ src/test/java/com/practice/EU7/DennisCodeEnvironment.java
@@ -0,0 +1,10 @@
+package com.practice.EU7;
+
+public class DennisCodeEnvironment {
+
+    public static void main(String[] args) {
+
+        System.out.println("welcome to Dennis branch !");
+    }
+
+}
--- src/test/java/com/practice/EU7/Practice.java
+++ src/test/java/com/practice/EU7/Practice.java
@@ -4,5 +4,9 @@ public class Practice {
     public static void main(String[] args) {
         System.out.println("practice");
         System.out.println("1");
+
+        System.out.println("We are lost in the project !!!");
+
+
     }
 }
